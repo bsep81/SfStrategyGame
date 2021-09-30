5723a851438d57a08486907e9be8bcd795dfa4c2
Borys <boria81@gmail.com>
2021-09-30 23:28:23 +0100

    Adds next turn button and method

--- src/main/java/controller/MainPaneController.java
+++ src/main/java/controller/MainPaneController.java
@@ -3,6 +3,9 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import model.Colony;
+import model.buildings.Alloyworks;
+import model.buildings.MetalMine;
 
 public class MainPaneController {
 
@@ -37,7 +40,26 @@ public class MainPaneController {
     private Label alloyworksCostLabel;
 
     @FXML
+    private Button nextTurnButton;
+
+    private Colony colony = Colony.builder()
+            .metal(500)
+            .alloys(500)
+            .metalMine(new MetalMine())
+            .alloyworks(new Alloyworks())
+            .build();
+
+    @FXML
     void initialize() {
+        nextTurnButton.setOnAction(event -> nextTurn());
+        metalLabel.textProperty().bind(colony.getMetalProperty());
+        alloysLabel.textProperty().bind(colony.getAlloysProperty());
+
+    }
+
+    private void nextTurn() {
+
+        colony.produceResources();
 
     }
 }
--- src/main/java/model/buildings/MetalMine.java
+++ src/main/java/model/buildings/MetalMine.java
@@ -1,34 +1,35 @@
 package model.buildings;
 
 
-import lombok.Builder;
+
 import lombok.Data;
 
 @Data
-@Builder
 public class MetalMine implements ProductionBuilding{
 
-    int level;
-    final int initialMetalCost;
-    final int initialAlloysCost;
-    final int baseProduction;
+    private int level = 0;
+    private static final int INITIAL_METAL_COST = 500;
+    private static final int INITIAL_ALLOYS_COST = 0;
+    private static final int BASE_PRODUCTION = 100;
+
 
     @Override
     public int produceResources() {
+
         if(level == 0) {
             return 0;
         }
-        return (int) (baseProduction * Math.pow(1.6, level));
+        return (int) (BASE_PRODUCTION * Math.pow(1.6, level));
     }
 
     @Override
     public int upgradeMetalCost() {
-        return (int) (initialMetalCost * Math.pow(2, level));
+        return (int) (INITIAL_METAL_COST * Math.pow(2, level));
     }
 
     @Override
     public int upgradeAlloysCost() {
-        return (int) (initialAlloysCost * Math.pow(2, level));
+        return (int) (INITIAL_ALLOYS_COST * Math.pow(2, level));
     }
 
     @Override
