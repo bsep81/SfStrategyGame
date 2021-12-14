483251d695e9d52ad809e6321928228ffa8651bc
Borys <boria81@gmail.com>
2021-12-14 23:23:35 +0000

    Add SpaceShipFactoryTest

--- src/main/java/model/spaceShips/SpaceShip.java
+++ src/main/java/model/spaceShips/SpaceShip.java
@@ -44,7 +44,4 @@ public abstract class SpaceShip {
         return target;
     }
 
-
-
-
 }
---
+++ src/test/java/model/spaceShips/SpaceShipFactoryTest.java
@@ -0,0 +1,47 @@
+package model.spaceShips;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpaceShipFactoryTest {
+
+    SpaceShipFactory factory = new SpaceShipFactory();
+
+    @Test
+    void shouldReturnNewFighter(){
+        SpaceShip fighter = new Fighter(new SpaceShipBaseParameters(SpaceShipType.FIGHTER, 50, 10, 40));
+
+        SpaceShip result = factory.createFighter();
+
+        assertThat(result).isEqualTo(fighter);
+    }
+
+    @Test
+    void shouldReturnNewCruiser(){
+        SpaceShip cruiser = new Cruiser(new SpaceShipBaseParameters(SpaceShipType.CRUISER, 200, 20, 70));
+
+        SpaceShip result = factory.createCruiser();
+
+        assertThat(result).isEqualTo(cruiser);
+    }
+
+    @Test
+    void shouldReturnNewDestroyer(){
+        SpaceShip destroyer = new Destroyer(new SpaceShipBaseParameters(SpaceShipType.DESTROYER, 300, 30, 150));
+
+        SpaceShip result = factory.createDestroyer();
+
+        assertThat(result).isEqualTo(destroyer);
+    }
+
+    @Test
+    void shouldReturnNewBomber(){
+        SpaceShip bomber = new Bomber(new SpaceShipBaseParameters(SpaceShipType.BOMBER, 250, 20, 160));
+
+        SpaceShip result = factory.createBomber();
+
+        assertThat(result).isEqualTo(bomber);
+    }
+
+}
